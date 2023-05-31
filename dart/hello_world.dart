07483d41ad6da49685091c5f429d10f966c97517
Jossie Esteban Rodriguez Salazar <jestebanrods@gmail.com>
2023-05-30 19:52:27 -0500

    feat: initial commit

---
+++ dart/hello_world.dart
@@ -0,0 +1,16 @@
+main() {
+  var myAge = 30;
+  print('Im $myAge Years Old');
+
+  String myName = 'Jossie';
+  print('Hello $myName');
+
+  late final String myLastName;
+  myLastName = 'Rodriguez';
+  print('Hello Mr. $myLastName');
+
+  const String myNick = 'Yoshi';
+  print('Hello $myNick');
+
+  print('Hello ${myNick.toUpperCase()}');
+}
